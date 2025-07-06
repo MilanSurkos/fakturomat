a2f4466642cbcaa9214cf52172ba2bebc0b78d33
Milan <surkos.milan@gmail.com>
2025-07-06 21:07:17 +0200

    Deploy test

--- project1/wsgi.py
+++ project1/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
 
 application = get_wsgi_application()
+
+# This is used by Render to run the application
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    import sys
+    
+    # Use PORT environment variable if available, otherwise default to 8000 for local development
+    port = os.environ.get('PORT', '8000')
+    execute_from_command_line([sys.argv[0], 'runserver', f'0.0.0.0:{port}'])
